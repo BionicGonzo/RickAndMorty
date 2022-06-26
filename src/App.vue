8569ff31e1412258bbedbae7459ce2a476a405f3
Gonzo <gonzalo.araosolguin@gmail.com>
2022-06-26 19:29:15 -0400

    Rick & Morty

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,41 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="rickandmorty min-vh-100 p-5 text-center">
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <div class="d-inline-flex p-3">
+      <h1 class="text-light">Rick and Morty</h1>
+    </div>
+    <Personaje :data="listaPersonajes"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import Personaje from "@/components/Personaje";
 
 export default {
-  name: 'App',
+  name: 'app',
   components: {
-    HelloWorld
+    Personaje
+  },
+  data(){
+    return {
+      listaPersonajes: []
+    }
+  },
+  methods: {
+    async llamarPersonajes(){
+      try {
+        const data = await fetch('https://rickandmortyapi.com/api/character')
+        const personajes = await data.json()
+        this.listaPersonajes = personajes.results
+        console.log(this.listaPersonajes)
+      } catch (error) {
+        console.warn(error)
+      }
+    }
+  },
+  created() { // al crear la instancia se llama a la función
+    this.llamarPersonajes()
   }
 }
 </script>
@@ -23,6 +47,13 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /* margin-top: 60px; */
+}
+
+.rickandmorty {
+  background-image: url("https://rickandmortypod.com/wp-content/uploads/2018/11/cropped-RM_page-header_background1-3.png");
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-attachment: fixed;
 }
 </style>
---
+++ src/components/Personaje.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="col-md-8 mx-auto">
+        <div v-for="(personaje, index) in data" :key="index" class="card bg-transparent border-success rounded-3 mb-3 p-3">
+            <div class="row">
+
+                <div class="col-2">
+                    <img :src="personaje.image" :alt="personaje.name" class="img-fluid rounded-circle">
+                </div>
+
+                <div class="col-10">
+                    <h2 class="text-start ms-3 fw-bold text-light">{{personaje.name}}</h2>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Personaje",
+    props: ['data'],
+    data() {
+        return {
+
+        }
+    },
+}
+</script>
+
+<style scoped>
+
+</style>
